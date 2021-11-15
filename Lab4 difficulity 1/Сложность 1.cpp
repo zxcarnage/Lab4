4c7ac1cd2358fc688d8b3d39e65fcfd420197a64
zxcarnage <nmoysenovich@gmail.com>
2021-11-15 13:11:22 +0300

    Diff. 1 finished

---
+++ Lab4 difficulity 1/Сложность 1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+int main() {
+
+	int arr[20], max_index = 0, size = 0, sum = 0;
+	cout << "Please, enter a size of our array" << endl;
+	cin >> size;
+	if (cin && size <= 20 && size > 0) {
+		cout << "Please, fill our array" << endl;
+
+		//Ввод массива
+		for (int i = 0; i < size; i++) {
+			cin >> arr[i];
+		}
+		
+		//Поиск максимального числа
+		for (int i = 0; i < size; i++) {
+			if (arr[max_index] < arr[i]) {
+				max_index = i;
+			}
+		}
+
+		//Подсчет суммы до масимального числа
+		for (int i = 0; i < max_index; i++) {
+			sum += arr[i];
+		}
+
+		cout << "The sum of the elements before max element is " << sum << endl;
+
+	}
+	else {
+		cout << "The size of array must be integer, over zero, less then 21" << endl;
+	}
+
+	return 0;
+}
