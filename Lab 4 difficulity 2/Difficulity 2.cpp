e563f1ca639d2ff7f26a0ac5ab09f69117cdee2a
zxcarnage <nmoysenovich@gmail.com>
2021-11-15 13:29:49 +0300

    Difficulity 2 fix

---
+++ Lab 4 difficulity 2/Difficulity 2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+int main() {
+	int size = 0, answer = 0, min = 0, sum = 0;
+	int arr[20];
+	srand(time(0));
+	cout << "Please enter the size of our array" << endl;
+	cin >> size;
+	cout << "How do you want to fill array?\n1.Manualy\n2.Randomly" << endl;
+	cin >> answer;
+	if (cin && size > 0 && size <= 20) {
+		switch (answer) {
+
+		case 1://Заполнение массива вручную
+			cout << "Please, enter the " << size << " elements manualy" << endl;
+			for (int i = 0; i < size; i++) {
+				cin >> arr[i];
+			}
+			break;
+
+		default://Заполнение массива рандомом
+		case 2:
+			cout << "You can relax, the random will do all for you" << endl;
+			for (int i = 0; i < size; i++) {
+				arr[i] = rand() % 10 + 1;
+				cout << "The " << i + 1 << " element of the array equals " << arr[i] << endl;
+			}
+			break;
+			break;
+		}
+
+		cout << "============================================================" << endl;
+
+		//Затирание нечетных элементов массива
+		for (int i = 0; i < size; i++) {
+			if (arr[i] % 2 == 0) {
+				for (int j = i; j < size; j++) {
+					arr[j] = arr[j + 1];
+				}
+				size--;
+			}
+		}
+
+		for (int i = 0; i < size; i++) {
+			cout << "The " << i + 1 << " element of the array equals " << arr[i] << endl;
+		}
+	}
+	return 0;
+}
